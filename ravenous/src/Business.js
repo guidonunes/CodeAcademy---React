3881e6064a439d912ac92857c74c73462f4d7c9c
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-22 18:04:55 -0300

    add export statement

--- ravenous/src/Business.js
+++ ravenous/src/Business.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
 function Business() {
-return(
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
+  return(
+    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
   )
 }
+
+
+export default Business;
